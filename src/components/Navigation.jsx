a87bda7d06267fff6fb381b91d21c3a80a8003bf
subat <subat.yucel1@gmail.com>
2025-09-17 13:11:26 +0300

    navigation responsive classes added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 function Navigation() {
   return (
     <header>
-      <nav className="bg-[#F7F7F7] px-[64px] py-[28px] flex items-center justify-between border-b border-[#F2F4F7]">
+      <nav className="bg-[#F7F7F7] px-8 py-5 md:px-16 md:py-7 flex items-center justify-between border-b border-[#F2F4F7]">
         <NavLink to="/" className="logo">
           <img src="/logo.png" alt="TravelTrucks logo" />
         </NavLink>
-        <ul className="flex gap-[32px] m-auto">
+        <ul className="flex gap-2.5 md:gap-8 md:m-auto">
           <li>
             <NavLink
               to="/"
