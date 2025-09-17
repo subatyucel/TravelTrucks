403ab106407ba61c57309d2cbf8c1ca58e34a16f
subat <subat.yucel1@gmail.com>
2025-09-17 12:59:31 +0300

    not found page svg implemented

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+import Navigation from "../components/Navigation";
+
 function NotFoundPage() {
-  return <div>This page does not exist!</div>;
+  return (
+    <>
+      <Navigation />
+      <div className="flex flex-col justify-center p-10 ">
+        <h1 className="text-center">
+          Looks like you missed the campsite. Let’s go{" "}
+          <Link to="/" className="activeLink underline">
+            home!
+          </Link>
+        </h1>
+        <svg className="w-full h-[50dvh] md:h-screen">
+          <use href="/notFound.svg" />
+        </svg>
+      </div>
+    </>
+  );
 }
 
 export default NotFoundPage;
